089d674ab130595e7ef7ef7f68a2fb3b2cdda984
Annajappa <123047530+Annajappa@users.noreply.github.com>
2025-06-14 09:16:15 +0530

    commit changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import Blog from "./Blog/page"
 
 export default function Home() {
   return (
-    <>
-    <Link href="/Main">Main</Link>
+    <div className="m-5">
+    <Link href="/Main" className="mr-4">Main</Link>
     <Link href="/Blog">Blog</Link>
-    </>
+    </div>
   );
 }
